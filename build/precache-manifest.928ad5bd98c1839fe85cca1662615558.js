e97f8f4102219bf92a20e3b61d512971a7f191dd
Alvin.Abhinav <alvin.abhinav@ithands.net>
2020-05-10 23:35:01 +0530

    first commit

---
+++ build/precache-manifest.928ad5bd98c1839fe85cca1662615558.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b724563b9260d07acd1aafede8cb27e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42f5ae4948e2ff5fce43",
+    "url": "/static/css/main.c02fc979.chunk.css"
+  },
+  {
+    "revision": "ae1c297116f67707baa5",
+    "url": "/static/js/2.990635ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.990635ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f5ae4948e2ff5fce43",
+    "url": "/static/js/main.f0a258a9.chunk.js"
+  },
+  {
+    "revision": "7ed131c7cc42c60e6c72",
+    "url": "/static/js/runtime-main.e88e8140.js"
+  },
+  {
+    "revision": "4c6b68b46c56e1ba112a58ddd2e5f077",
+    "url": "/static/media/about-extra-1.4c6b68b4.svg"
+  },
+  {
+    "revision": "289a1e0eb327b3d7ebee60ab1b296fc8",
+    "url": "/static/media/about-img.289a1e0e.svg"
+  },
+  {
+    "revision": "aa3529529332f23148a6863329565526",
+    "url": "/static/media/client-1.aa352952.png"
+  },
+  {
+    "revision": "e5abdf9fc95ee8e631e123d0ac889fe5",
+    "url": "/static/media/client-2.e5abdf9f.png"
+  },
+  {
+    "revision": "d74691a96c51bc29bb4f0e1261c51ca7",
+    "url": "/static/media/client-3.d74691a9.png"
+  },
+  {
+    "revision": "fb49d92b2cbaff92dc910b88483fdf69",
+    "url": "/static/media/client-4.fb49d92b.png"
+  },
+  {
+    "revision": "ef2f2c0e89a1e5c2b44343acbb6fbc85",
+    "url": "/static/media/client-5.ef2f2c0e.png"
+  },
+  {
+    "revision": "20e8f8600db51020010f62b7cc172419",
+    "url": "/static/media/intro-bg.20e8f860.png"
+  },
+  {
+    "revision": "2586459b5b0c9acd5320bc38cd3e22fc",
+    "url": "/static/media/intro-img.2586459b.svg"
+  },
+  {
+    "revision": "d2276eb36fea3270121625e19c2e17f7",
+    "url": "/static/media/team-1.d2276eb3.jpg"
+  },
+  {
+    "revision": "77c3951defcb6e3d5d8369ef908bff25",
+    "url": "/static/media/team-2.77c3951d.jpg"
+  },
+  {
+    "revision": "92900f240e82e5cebb6c1393e6485285",
+    "url": "/static/media/team-3.92900f24.jpg"
+  },
+  {
+    "revision": "2a7486d664b3537bc6ae81ff9bdb0878",
+    "url": "/static/media/team-4.2a7486d6.jpg"
+  }
+]);
